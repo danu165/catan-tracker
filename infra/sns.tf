3e2fc958c7482f9af8e0b015bac455d248b977f4
danu165 <>
2024-01-07 15:38:44 -0800

    add sns policy

--- infra/sns.tf
+++ infra/sns.tf
@@ -1,3 +1,64 @@
 resource "aws_sns_topic" "sns" {
   name = "${local.project_name}-${local.branch_hash}"
+  delivery_policy = <<EOF
+  {
+    "http": {
+      "defaultHealthyRetryPolicy": {
+        "minDelayTarget": 20,
+        "maxDelayTarget": 20,
+        "numRetries": 3,
+        "numMaxDelayRetries": 0,
+        "numNoDelayRetries": 0,
+        "numMinDelayRetries": 0,
+        "backoffFunction": "linear"
+      },
+      "disableSubscriptionOverrides": false,
+      "defaultThrottlePolicy": {
+        "maxReceivesPerSecond": 1
+      }
+    }
+  }
+EOF
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "${local.project_name}_sns_logging-${local.branch_hash}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name   = "policy-8675309"
+    policy = data.aws_iam_policy_document.inline_policy.json
+  }
+}
+
+data "aws_iam_policy_document" "inline_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+      "logs:PutRetentionPolicy"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_sns_sms_preferences" "update_sms_prefs" {
+  monthly_spend_limit          = 5
+  delivery_status_iam_role_arn = aws_iam_role.test_role.arn
+  default_sms_type             = "Promotional"  # Transactional is more expensive
 }
